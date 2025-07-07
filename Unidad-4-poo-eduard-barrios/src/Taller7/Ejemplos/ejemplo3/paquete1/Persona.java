0645116d6821ebf87330bc04b5617585bb809c4a
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 14:13:31 -0500

    feat: eduard barrios - ejemplo 3 taller 7

---
+++ Unidad-4-poo-eduard-barrios/src/Taller7/Ejemplos/ejemplo3/paquete1/Persona.java
@@ -0,0 +1,8 @@
+package Taller7.Ejemplos.ejemplo3.paquete1;
+
+class Persona {
+    public String nombre;
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ Unidad-4-poo-eduard-barrios/src/Taller7/Ejemplos/ejemplo3/paquete2/main.java
@@ -0,0 +1,12 @@
+package Taller7.Ejemplos.ejemplo3.paquete2;
+
+
+//La siguiente línea genera un error de compilación porque
+//la clase Persona no es pública y por lo tanto no es visible aquí.
+/*
+import paquete1.Persona;
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan"); // Error de compilación: Persona no es visible
+    }
+}*/
