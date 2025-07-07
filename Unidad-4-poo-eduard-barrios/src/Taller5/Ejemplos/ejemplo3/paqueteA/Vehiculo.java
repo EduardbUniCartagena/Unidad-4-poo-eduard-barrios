54b0f58aa457c6787d5530d8c575180ec80e3215
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 09:15:34 -0500

    feat: eduard barrios - ejemplo 3 taller 5

---
+++ Unidad-4-poo-eduard-barrios/src/Taller5/Ejemplos/ejemplo3/paqueteA/Vehiculo.java
@@ -0,0 +1,9 @@
+package Taller5.Ejemplos.ejemplo3.paqueteA;
+
+class Vehiculo {
+    String marca;
+    Vehiculo(String marca) {
+        this.marca = marca;
+    }
+}
+
---
+++ Unidad-4-poo-eduard-barrios/src/Taller5/Ejemplos/ejemplo3/paqueteB/PruebaVehiculo.java
@@ -0,0 +1,17 @@
+//al decomentar el siguiente bloque de codigo obtendremos un error de compilacion debidoa que esta clase no tiene acceso a la clase vehiculo que se encuentra en el paquete A
+
+/*package Taller5.Ejemplos.ejemplo3.paqueteB;
+import paqueteA.Vehiculo;
+
+
+public class PruebaVehiculo {
+    // Paquete: paqueteB
+
+    public class Main {
+        public static void main(String[] args) {
+            Vehiculo v = new Vehiculo("Toyota"); // Error de compilación: Vehiculo no es visible
+        }
+    }
+}*/
+
+
