8205b9afaf97d42a83eb029749997e148d1a1cf3
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 13:57:46 -0500

    feat: eduard barrios - ejemplo 2 taller 7

---
+++ Unidad-4-poo-eduard-barrios/src/Taller7/Ejemplos/ejemplo2/PruebaUtilidades.java
@@ -0,0 +1,8 @@
+package Taller7.Ejemplos.ejemplo2;
+
+public class PruebaUtilidades {
+    public static void main(String[] args) {
+        // Se llama al metodo estático directamente desde la clase Utilidades
+        Utilidades.imprimirMensaje("Hola, este es un mensaje desde la clase de prueba.");
+    }
+}
---
+++ Unidad-4-poo-eduard-barrios/src/Taller7/Ejemplos/ejemplo2/Utilidades.java
@@ -0,0 +1,7 @@
+package Taller7.Ejemplos.ejemplo2;
+
+public class Utilidades {
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
