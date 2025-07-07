69997ba2d1a2875b35fbf254a70d3788e2ed7dd7
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 10:19:36 -0500

    feat: eduard barrios - ejemplo 3 taller 6

---
+++ Unidad-4-poo-eduard-barrios/src/Taller6/Ejemplos/ejemplo3/paquete2/main.java
@@ -0,0 +1,10 @@
+package Taller6.Ejemplos.ejemplo3.paquete2;
+//al descomentar el codigo tenemos un error debido a que esta clase no tiene acceso a la clase persona
+/*
+import paquete1.Persona;
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Juan");
+        System.out.println(p.nombre); // Error de compilación: nombre tiene acceso protegido
+    }
+}*/
