ca1218410c53d32de3057ab5bfed7d2ad590876d
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 21:31:42 -0500

    feat: eduard barrios - ejercicio 3 taller 11

---
+++ Unidad-4-poo-eduard-barrios/src/Taller11/Ejercicios/ejercicio3/Herramienta.java
@@ -0,0 +1,8 @@
+package Taller11.Ejercicios.ejercicio3;
+
+//Clase abstracta
+public abstract class Herramienta {
+    public abstract void usar();
+}
+
+
---
+++ Unidad-4-poo-eduard-barrios/src/Taller11/Ejercicios/ejercicio3/PruebaHerramienta.java
@@ -0,0 +1,9 @@
+package Taller11.Ejercicios.ejercicio3;
+
+
+public class PruebaHerramienta {
+    public static void main(String[] args) {
+        //La siguiente línea dará un error de compilación
+        //Herramienta h = new Herramienta();
+    }
+}
