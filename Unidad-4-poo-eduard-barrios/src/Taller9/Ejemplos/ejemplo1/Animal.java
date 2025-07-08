e4b41e30c2f7e380437566e5d24b036ece17ce5b
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 19:11:38 -0500

    feat: eduard barrios - ejemplo 1 taller 9

---
+++ Unidad-4-poo-eduard-barrios/src/Taller9/Ejemplos/ejemplo1/Animal.java
@@ -0,0 +1,8 @@
+package Taller9.Ejemplos.ejemplo1;
+
+public class Animal {
+    protected String especie;
+    public Animal(String especie) {
+        this.especie = especie;
+    }
+}
---
+++ Unidad-4-poo-eduard-barrios/src/Taller9/Ejemplos/ejemplo1/PerroPrueba.java
@@ -0,0 +1,9 @@
+package Taller9.Ejemplos.ejemplo1;
+
+public class PerroPrueba {
+    public static void main(String[] args) {
+        //Se crea una instancia de la subclase
+        Perro miPerro = new Perro("canino", "pitbul");
+
+    }
+}
