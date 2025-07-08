1d9ab3823dd7ecc8dae95c1c4824e337755ce62c
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 21:40:27 -0500

    feat: eduard barrios - ejemplo 1 taller 12

---
+++ Unidad-4-poo-eduard-barrios/src/Taller12/Ejemplos/ejemplo1/Nadador.java
@@ -0,0 +1,6 @@
+package Taller12.Ejemplos.ejemplo1;
+
+// Interfaz Nadador
+public interface Nadador {
+    void nadar();
+}
---
+++ Unidad-4-poo-eduard-barrios/src/Taller12/Ejemplos/ejemplo1/Volador.java
@@ -0,0 +1,6 @@
+package Taller12.Ejemplos.ejemplo1;
+
+// Interfaz Volador
+public interface Volador {
+    void volar();
+}
