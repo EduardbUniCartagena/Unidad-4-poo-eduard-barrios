b54d28b7a3dab6c5076873cd233a7f83584b05a2
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 21:15:14 -0500

    feat: eduard barrios - ejemplo 2 taller 11

---
+++ Unidad-4-poo-eduard-barrios/src/Taller11/Ejemplos/ejemplo2/Coche.java
@@ -0,0 +1,7 @@
+package Taller11.Ejemplos.ejemplo2;
+//al descomentar el siguiente bloque de codigo obtendremos el error
+/*
+// Clase derivada incorrecta
+public class Coche extends Vehiculo {
+// Error de compilación: La clase Coche debe implementar el metodo abstracto conducir()
+}*/
---
+++ Unidad-4-poo-eduard-barrios/src/Taller11/Ejemplos/ejemplo2/Vehiculo.java
@@ -0,0 +1,6 @@
+package Taller11.Ejemplos.ejemplo2;
+
+// Clase abstracta
+public abstract class Vehiculo {
+    public abstract void conducir(); // Metodo abstracto
+}
