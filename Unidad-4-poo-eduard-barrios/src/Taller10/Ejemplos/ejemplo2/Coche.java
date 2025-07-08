e9e517c2da45f071a9628c11b13b2472801daafa
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 19:37:32 -0500

    feat: eduard barrios - ejemplo 2 taller 10

---
+++ Unidad-4-poo-eduard-barrios/src/Taller10/Ejemplos/ejemplo2/Coche.java
@@ -0,0 +1,10 @@
+package Taller10.Ejemplos.ejemplo2;
+
+// Clase derivada (incorrecta)
+public class Coche extends Vehiculo {
+    // Error de compilación: Diferente firma del metodo
+//    /*@Override
+//    public void acelerar() {
+//        System.out.println("El coche acelera.");
+//    }*/
+}
