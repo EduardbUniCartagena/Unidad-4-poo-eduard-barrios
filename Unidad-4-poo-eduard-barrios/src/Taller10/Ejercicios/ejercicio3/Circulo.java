7942e4940899ab4f5104ccb287216dcd48d1b9b1
Eduard David Barrios Padilla <ebarriosp2@unicartagena.edu.co>
2025-07-07 19:48:23 -0500

    feat: eduard barrios - ejercicio 3 taller 10

---
+++ Unidad-4-poo-eduard-barrios/src/Taller10/Ejercicios/ejercicio3/Circulo.java
@@ -0,0 +1,10 @@
+package Taller10.Ejercicios.ejercicio3;
+
+public class Circulo extends Figura {
+    //ERROR: Se intenta sobrescribir un metodo con una firma diferente.
+    //'dibujar()' no es lo mismo que 'dibujar(String color)'.
+   /* @Override
+    public void dibujar() {
+        System.out.println("Dibujando un círculo.");
+    }*/
+}
---
+++ Unidad-4-poo-eduard-barrios/src/Taller10/Ejercicios/ejercicio3/Figura.java
@@ -0,0 +1,8 @@
+// Archivo: Figura.java
+package Taller10.Ejercicios.ejercicio3;
+
+public class Figura {
+    public void dibujar(String color) {
+        System.out.println("Dibujando una figura de color " + color);
+    }
+}
